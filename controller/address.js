7249e2123283cdbc248a85d5aa5467598fa79ce9
briancatraguna-gc <brian.catraguna@gc.com>
2023-10-25 00:22:28 -0400

    Added get all states endpoint

--- controller/address.js
+++ controller/address.js
@@ -1,5 +1,8 @@
+const { validationResult } = require('express-validator');
+
 const RegionAccessor = require('../model/region');
 const CountryAccessor = require('../model/country');
+const StateAccessor = require('../model/state');
 
 exports.getRegions = async (req, res, next) => {
     const regions = await RegionAccessor.getAll();
@@ -9,4 +12,17 @@ exports.getRegions = async (req, res, next) => {
 exports.getCountries = async (req, res, next) => {
     const countries = await CountryAccessor.getAll();
     return res.status(200).json(countries);
+}
+
+exports.getStates = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+		return res.status(400).json({
+            message: "Validation error",
+            errors: errors.array()
+        });
+	}
+    const countryId = req.params.countryId;
+    const states = await StateAccessor.getAllByCountry(countryId);
+    return res.status(200).json(states);
 }
--- routes/address.js
+++ routes/address.js
@@ -1,12 +1,33 @@
 const express = require('express');
+const { param } = require('express-validator');
+
 const addressController = require('../controller/address');
 
 const router = express.Router();
 
 const isAuthenticated = require("../middleware/is-auth");
 
-router.get('/regions', isAuthenticated, addressController.getRegions);
+router.get(
+    '/regions', 
+    isAuthenticated, 
+    addressController.getRegions
+);
 
-router.get('/countries', isAuthenticated, addressController.getCountries);
+router.get(
+    '/countries', 
+    isAuthenticated, 
+    addressController.getCountries
+);
+
+router.get(
+    '/states/:countryId',
+    isAuthenticated, 
+    [
+        param('countryId')
+            .trim()
+            .isInt(),
+    ],
+    addressController.getStates
+);
 
 module.exports = router;

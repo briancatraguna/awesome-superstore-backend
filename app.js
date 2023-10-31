54387c3313eb9fb8208a54bd7a91e27fbfec6ca2
briancatraguna-gc <brian.catraguna@gc.com>
2023-10-30 23:29:58 -0400

    Added extra cors config

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ const app = express();
 //  CORS
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Method", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header('Access-Control-Allow-Credentials', 'true'); // If you need to allow credentials
   next();
 });
 
